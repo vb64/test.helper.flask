d1f93d71a19f054a9ad3cc68ceecb968f602fbc5
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2023-04-12 09:13:52 +0400

    Add pep257 tests

--- tester_flask/__init__.py
+++ tester_flask/__init__.py
@@ -1,4 +1,5 @@
-"""
+"""Test helper for Flask app.
+
 http://flask.pocoo.org/docs/1.0/testing/
 https://cloud.google.com/appengine/docs/standard/python/getting-started/python-standard-env
 """
@@ -11,37 +12,30 @@ except ImportError:  # pragma: no cover
 
 
 class RequestFactory:  # pylint: disable=too-few-public-methods
-    """
-    requests generator
-    """
+    """Generator of the requests."""
+
     def __init__(self, app):
+        """Save Flask app instance."""
         self.app = app
 
     def post(self, url, data='', content_type='application/octet-stream'):
-        """
-        post requests tests
-        """
+        """Post requests tests."""
         return self.app.test_request_context(url, data=data, method='POST', content_type=content_type)
 
 
 class TestFlask(unittest.TestCase):
-    """
-    Flask apps tester
-    """
+    """Flask apps tester."""
+
     def setUp(self, app):  # pylint: disable=arguments-differ
+        """Set defaults for Flask testing."""
         unittest.TestCase.setUp(self)
         self.app = app
         self.app.config['TESTING'] = True
         self.client = self.app.test_client()
         self.factory = RequestFactory(self.app)
 
-    def tearDown(self):
-        unittest.TestCase.tearDown(self)
-
     def get_url(self, view_name, **kwargs):
-        """
-        generate url for view in app test context
-        """
+        """Generate url for view in app test context."""
         with self.app.test_request_context():
             from flask import url_for
             url = url_for(view_name, **kwargs)
@@ -49,9 +43,7 @@ class TestFlask(unittest.TestCase):
         return url
 
     def guest_view(self, url, return_code=200, follow_redirects=False):
-        """
-        generic get request
-        """
+        """Generic get request."""
         response = self.client.get(url, follow_redirects=follow_redirects)
 
         self.assertEqual(
@@ -62,9 +54,7 @@ class TestFlask(unittest.TestCase):
         return response
 
     def simple_view(self, view_name, return_code=200, follow=False, get_param=None):
-        """
-        simple get request
-        """
+        """Simple get request."""
         url = self.get_url(view_name)
         if get_param:
             url = url + "?" + urlencode(get_param)
@@ -73,35 +63,27 @@ class TestFlask(unittest.TestCase):
     def param_view(
       self, view_name, params, return_code=200, follow=False, get_param=None
     ):  # pylint: disable=too-many-arguments
-        """
-        get request with parameters
-        """
+        """Get request with parameters."""
         url = self.get_url(view_name, **params)
         if get_param:
             url = url + "?" + urlencode(get_param)
         return self.guest_view(url, return_code=return_code, follow_redirects=follow)
 
     def simple_post(self, view_name, post_dict, follow=True, content_type=None):
-        """
-        simple post request
-        """
+        """Simple post request."""
         url = self.get_url(view_name)
         return self.client.post(url, data=post_dict, follow_redirects=follow, content_type=content_type)
 
     def param_post(
       self, view_name, params, post_dict, follow=True, get_param=None, content_type=None
     ):  # pylint: disable=too-many-arguments
-        """
-        post request with parameters
-        """
+        """Post request with parameters."""
         url = self.get_url(view_name, **params)
         if get_param:
             url = url + "?" + urlencode(get_param)
         return self.client.post(url, data=post_dict, follow_redirects=follow, content_type=content_type)
 
     def final_url(self, response):
-        """
-        return final url after redirect
-        """
-        self.assertEqual(response.status_code, 302)
+        """Return final url after redirect."""
+        assert response.status_code == 302
         return urlparse(response.location).path
